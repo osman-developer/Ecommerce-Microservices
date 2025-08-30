c141af7343196bf787860c0f397bb45dd363cecf
osman-developer <57558743+osman-developer@users.noreply.github.com>
2025-08-30 08:10:28 +0300

    Adding common files

---
+++ ECommerce.SharedLibrarySln/ECommerce.SharedLibrary/Logs/LogException.cs
@@ -0,0 +1,27 @@
+﻿namespace ECommerce.Common.Logs
+{
+    public static class LogException
+    {
+        public static void LogExceptions(Exception ex)
+        {
+            LogToFile(ex.Message);
+            LogToConsole(ex.Message);
+            LogToDebugger(ex.Message);
+        }
+
+        private static void LogToDebugger(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void LogToConsole(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void LogToFile(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ECommerce.SharedLibrarySln/ECommerce.SharedLibrary/Response/Response.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ECommerce.Common.Response
+{
+    public record Response<T>
+    {
+        public bool Success { get; init; }
+        public string Message { get; init; } = string.Empty;
+        public T? Data { get; init; }
+
+        public static Response<T> Ok(T data, string message = "") =>
+            new() { Success = true, Data = data, Message = message };
+
+        public static Response<T> Fail(string message) =>
+            new() { Success = false, Data = default, Message = message };
+    }
+
+}
